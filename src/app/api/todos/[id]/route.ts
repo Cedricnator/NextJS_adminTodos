a844bbd27c43670bba4fd36364f7e11cc50535bb
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-03-17 17:21:27 -0300

    feat(PUT): added the put route to one todo

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,6 +1,7 @@
 import prisma from '@/lib/prisma';
 import { NextResponse, NextRequest} from 'next/server';
 
+// URL
 interface Segments {
     params: {
         id: string;
@@ -15,10 +16,30 @@ export async function GET(request: Request, { params }: Segments ) {
             id: params.id
         }
     })
-    
-    if(!oneTodo) return NextResponse.json({ message: `No todo found with the id ${ params.id }`}, { status: 404 })
+    // sino hay todo, retorna error
+    if(!oneTodo) 
+        return NextResponse.json({ message: `No todo found with the id ${ params.id }`}, { status: 404 })
 
     return NextResponse.json( oneTodo )
 } 
 
 
+export async function PUT( request: Request, { params }: Segments ){
+    // busca todo
+    const { id } = params;
+    const todo = await prisma.todo.findFirst({ where: { id }})
+
+    // sino hay todo, retorna error
+    if( !todo ){
+        return NextResponse.json({ message: `No todo found with the id ${ id }`}, { status: 404 }) 
+    }
+
+    const body = await request.json();
+
+    const updatedTodo = await prisma.todo.update({
+        where: { id },
+        data: body
+    })
+
+    return NextResponse.json( updatedTodo )
+}
