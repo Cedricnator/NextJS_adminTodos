abfe5d800bf788f4482f4e89642606ed2c6ff925
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-03-17 17:42:29 -0300

    refactor

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,7 @@
 import prisma from '@/lib/prisma';
 import { NextResponse, NextRequest} from 'next/server';
+import * as yup from 'yup';
+import { Todo } from '@prisma/client';
 
 // URL
 interface Segments {
@@ -8,38 +10,56 @@ interface Segments {
     }
 }
 
+const getOneTodo = async( id:string ): Promise<Todo | null> => {
+    return await prisma.todo.findFirst({ where: { id }});
+}
+
 // Con segmentos podremos ver los parametros de la peticion
 export async function GET(request: Request, { params }: Segments ) {
 
-    const oneTodo = await prisma.todo.findUnique({
-        where: {
-            id: params.id
-        }
-    })
+    const oneTodo = getOneTodo(params.id)
+
     // sino hay todo, retorna error
-    if(!oneTodo) 
-        return NextResponse.json({ message: `No todo found with the id ${ params.id }`}, { status: 404 })
+    if(!oneTodo){ 
+        return NextResponse.json({ 
+            message: `No todo found with the id ${ params.id }`
+        },{ 
+            status: 404 
+        })
+    }
 
     return NextResponse.json( oneTodo )
 } 
+
+//* Validacion *// 
+const putSchema = yup.object({
+    complete: yup.boolean().optional(),
+    description: yup.string().optional()
+})
 
 
 export async function PUT( request: Request, { params }: Segments ){
     // busca todo
     const { id } = params;
-    const todo = await prisma.todo.findFirst({ where: { id }})
+    const todo = getOneTodo( id );
 
     // sino hay todo, retorna error
     if( !todo ){
         return NextResponse.json({ message: `No todo found with the id ${ id }`}, { status: 404 }) 
     }
 
-    const body = await request.json();
+    try {
+        const { complete, description } = await putSchema.validate(request.json());
 
-    const updatedTodo = await prisma.todo.update({
-        where: { id },
-        data: body
-    })
+        const updatedTodo = await prisma.todo.update({
+            where: { id },
+            data: { complete, description }
+        })
 
-    return NextResponse.json( updatedTodo )
+        return NextResponse.json( updatedTodo )
+    } catch (error) {
+        return NextResponse.json(error, { status: 400 })
+    }
+
+   
 }
