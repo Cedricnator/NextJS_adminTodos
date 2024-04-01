78cd4ad9ac5c3bf09ba372df7c005c1f73097f8a
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-04-01 09:10:18 -0300

    feat: styles for todo's

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,8 +1,31 @@
+// 'use client';
+// import { useEffect } from "react";
+import prisma from "@/lib/prisma"
+import { TodosGrid } from "@/todos";
 
-export default function RestTodosPage() {
+
+export const metadata = {
+  title: 'Listado de todos',
+  description: 'Listado de todos los todos',
+}
+
+export default async function RestTodosPage() {
+  
+  //* Server Action
+  const todos = await prisma.todo.findMany({orderBy: { description: 'asc' }});
+  
+  //* Forma tradicional de hacerlo
+  // useEffect(() => {
+  //   fetch('/api/todos')
+  //     .then( resp => resp.json() )
+  //     .then( console.log )  
+  
+  // }, [])
+  
+  
   return (
     <div>
-      Hola aaa
+      <TodosGrid todos={ todos } />
     </div>
   )
 }
